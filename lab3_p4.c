8ca45d3d4e8cff6b5486ec0828c2fee2d3a3cb0c
Padariya-Pranav <pranavpadariya007@gmail.com>
2025-08-29 10:06:42 +0530

    Add files via upload

---
+++ lab3_p4.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+void main()
+
+{
+    int x;
+    printf("Enter the value of X:\n");
+    scanf("%d", &x);
+
+    printf("The first %d natural numbers are:\n", x);
+    for(int a=1 ; a<=x ; a++ )
+    {
+        printf("%d\n", a);
+
+
+    }
+
+
+
+}
